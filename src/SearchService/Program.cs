8bcc44e542b4d9f9d9e13241afcee859fbd21ffc
amranjaycar <rajib.mrn@gmail.com>
2023-12-16 10:47:14 +1100

    Update - Gateway Service and Docker added.

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -23,6 +23,12 @@ builder.Services.AddMassTransit(x =>
     x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("Search", false));
     x.UsingRabbitMq((context, cfg) =>
     {
+        cfg.Host(builder.Configuration["RabbitMq:Host"], "/", h =>
+        {
+            h.Username(builder.Configuration.GetValue("RabbitMq:Username", "guest"));
+            h.Password(builder.Configuration.GetValue("RabbitMq:Password", "guest"));
+        });
+        
         cfg.ReceiveEndpoint("search-auction-created", e => 
         {
             e.UseMessageRetry(r => r.Interval(5,5));
